6c5e783e385f62ae06fb2949c9bb45a1811792d7
Deeksha Sharma <deekshashrm032@gmail.com>
2024-09-18 18:13:16 +0530

    adding question.js

--- question.js
+++ question.js
@@ -4,3 +4,7 @@
 //what async/await , try/catch, 
 //what is the differnce between async/await and try catch
 // what is event driven architecture 
+// why Java Script or node js fast
+// what  middelware
+// How to handle error
+// which types  of error  can be 
