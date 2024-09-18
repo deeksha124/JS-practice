f1c4c686a4e3fdf9a324d28f0a05059c37b62263
Deeksha Sharma <deekshashrm032@gmail.com>
2024-09-18 17:53:47 +0530

    Add Question file

---
+++ question.js
@@ -0,0 +1,6 @@
+//What is pure function
+//what is promises,how its work
+//what is events
+//what async/await , try/catch, 
+//what is the differnce between async/await and try catch
+// what is event driven architecture 
